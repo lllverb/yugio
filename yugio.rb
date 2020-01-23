4e89197687fda5895d55d3f2c5223ad424648e9b
lllverb <54735495+lllverb@users.noreply.github.com>
2020-01-24 02:36:41 +0900

    Initial commit

---
+++ yugio.rb
@@ -0,0 +1,31 @@
+class Monster
+  attr_accessor :name, :attack, :defense, :star
+  def initialize(**params)
+    @name = params[:name]
+    @attack = params[:attack]
+    @defense = params[:defense]
+    @star = params[:star]
+  end
+end
+
+def summon(hands, input)
+  puts "#{hands[input - 1].name}を召喚!"
+end
+
+monsters = [{name: "砦を守る翼竜", attack: 1400, defense: 1200, star: 4}, {name: "デーモンの召喚", attack: 2500, defense: 1200, star: 6}]
+card1 = Monster.new(monsters[0])
+card2 = Monster.new(monsters[1])
+hands = []
+hands << card1
+hands << card2
+puts "----------------------------"
+hands.each_with_index do |hand, i|
+  puts "手札#{i + 1}: #{hand.name}"
+end
+# puts "手札1: #{hands[0].name}\n手札2: #{hands[1].name}"
+puts "----------------------------"
+# puts "どちらを召喚しますか?"
+# puts "手札1:[1]\n手札2:[2]"
+# input = gets.to_i
+# summon(hands, input)
+
